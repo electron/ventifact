08191d27631966bd83d9ad0dd972deed08ab95ad
clavin <clavin@electronjs.org>
2023-08-10 19:33:23 -0600

    add date to pr statuses page

--- packages/web/src/routes/pr-statuses/+page.svelte
+++ packages/web/src/routes/pr-statuses/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Temporal } from '@js-temporal/polyfill';
 	import type { PageData } from './$types';
 	import Chart from './Chart.svelte';
 	import type { PR } from 'data-lib';
@@ -17,28 +18,32 @@
 
 	const PASSING_GOAL = 0.8;
 
-	$: [passingPct, failingPct] = calculatePassingAndFailingPercentages(rehydratedData);
+	$: [date, passingPct, failingPct] = calculateLatestPassingAndFailingPercentages(rehydratedData);
 	$: meetingGoal = passingPct !== undefined && passingPct >= PASSING_GOAL;
 
-	function calculatePassingAndFailingPercentages(
+	function calculateLatestPassingAndFailingPercentages(
 		data: OrderedMap<string, Map<PR['status'], number>>
-	): [number | undefined, number | undefined] {
-		const counts = data.last();
+	): [string, number | undefined, number | undefined] {
+		const lastEntry = data.entrySeq().last();
 
 		// If there is no data, return undefined
-		if (counts === undefined) {
-			return [undefined, undefined];
+		if (lastEntry === undefined) {
+			return ['', undefined, undefined];
 		}
+
+		const [date, counts] = lastEntry;
+
+		const dateStr = Temporal.PlainDate.from(date).toLocaleString();
 
 		const passing = counts.get('success', 0);
 		const failing = counts.get('failure', 0);
 
 		// Protect against a division by zero
 		if (passing === 0 && failing === 0) {
-			return [0, 0];
+			return [dateStr, 0, 0];
 		} else {
 			const total = passing + failing;
-			return [passing / total, failing / total];
+			return [dateStr, passing / total, failing / total];
 		}
 	}
 
@@ -55,27 +60,32 @@
 
 	<div class="flex flex-col gap-2">
 		{#if passingPct !== undefined && failingPct !== undefined}
-			<div class="flex flex-row flex-wrap gap-4">
-				{#if passingPct !== undefined}
-					<div
-						class={`p-4 border-2 rounded-lg border-green-500 ${
-							meetingGoal ? 'bg-green-500 text-white' : ''
-						}`}
-					>
-						<div class="font-bold text-md">Passing</div>
-						<div class="font-light text-4xl">{formatPct(passingPct)}</div>
-					</div>
-				{/if}
-				{#if failingPct !== undefined}
-					<div
-						class={`p-4 border-2 rounded-lg border-red-500 ${
-							meetingGoal ? '' : 'bg-red-500 text-white'
-						}`}
-					>
-						<div class="font-bold text-md">Failing</div>
-						<div class="font-light text-4xl">{formatPct(failingPct)}</div>
-					</div>
-				{/if}
+			<div class="flex flex-col gap-1">
+				<div>
+					<span class="font-bold text-md">Latest</span> ({date})
+				</div>
+				<div class="flex flex-row flex-wrap gap-4">
+					{#if passingPct !== undefined}
+						<div
+							class={`p-4 border-2 rounded-lg border-green-500 ${
+								meetingGoal ? 'bg-green-500 text-white' : ''
+							}`}
+						>
+							<div class="font-bold text-md">Passing</div>
+							<div class="font-light text-4xl">{formatPct(passingPct)}</div>
+						</div>
+					{/if}
+					{#if failingPct !== undefined}
+						<div
+							class={`p-4 border-2 rounded-lg border-red-500 ${
+								meetingGoal ? '' : 'bg-red-500 text-white'
+							}`}
+						>
+							<div class="font-bold text-md">Failing</div>
+							<div class="font-light text-4xl">{formatPct(failingPct)}</div>
+						</div>
+					{/if}
+				</div>
 			</div>
 		{/if}
 
